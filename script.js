b147a9fe64fe705b13fb455bd038b458f0014ac3
EasyProPer <k.o.3.a.4.o.kxxl@gmail.com>
2025-10-20 10:03:43 +0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+const dayNamesRU = [
+  'Воскресенье',
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота'
+];
+
+const monthNamesRU = [
+  'Январь',
+  'Февраль',
+  'Март',
+  'Апрель',
+  'Май',
+  'Июнь',
+  'Июль',
+  'Август',
+  'Сентябрь',
+  'Октябрь',
+  'Ноябрь',
+  'Декабрь'
+];
+
+const currentDate = new Date();  // объект даты
+
+const dayTitle = document.querySelector('.day-title');
+dayTitle.textContent = dayNamesRU[currentDate.getDay()];
+
+const dayDate = document.querySelector('.day-date');
+const day = currentDate.getDate().toString().padStart(2, '0');  // день
+const month = monthNamesRU[currentDate.getMonth()];             // месяц
+const year = currentDate.getFullYear();                         // год
+dayDate.textContent = `${day} ${month} ${year}`;
+const tg = window.Telegram.WebApp;
+const userName = document.querySelector('.user-name');
+
+userName.textContent = tg.initDataUnsafe.user.first_name;
+const userPhoto = document.querySelector('.user-photo > img');
+
+userPhoto.setAttribute('src', tg.initDataUnsafe.user.photo_url);
